0df1faa0037036d14226f241c271250d7e01795a
nothingtosay920 <963717822@qq.com>
2020-11-25 20:15:49 +0800

    11.25

---
+++ leedcode/树/144.js
@@ -0,0 +1,8 @@
+let preorderTraversal  = (root, arr = []) => {
+    if(root) {
+      arr.push(root.val)
+      preorderTraversal(root.left, arr)
+      preorderTraversal(root.right, arr)
+    }
+    return arr
+  }
