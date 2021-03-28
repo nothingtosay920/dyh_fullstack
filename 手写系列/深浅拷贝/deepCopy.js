78dddafd0f862ed882147fc00122f6cdd5a7ca15
nothingtosay920 <963717822@qq.com>
2021-03-28 23:09:29 +0800

    3.28

--- 手写系列/深浅拷贝/deepCopy.js
+++ 手写系列/深浅拷贝/deepCopy.js
@@ -10,3 +10,29 @@ let deepCopy = function(obj) {
     return new_obj
   }
 }
+
+
+function deepClone(source){
+  const targetObj = source.constructor === Array ? [] : {}; // 判断复制的目标是数组还是对象
+  for(let keys in source){ // 遍历目标
+    if(source.hasOwnProperty(keys)){
+      if(source[keys] && typeof source[keys] === 'object'){ // 如果值是对象，就递归一下
+        targetObj[keys] = source[keys].constructor === Array ? [] : {};
+        targetObj[keys] = deepClone(source[keys]);
+      }else{ // 如果不是，就直接赋值
+        targetObj[keys] = source[keys];
+      }
+    } 
+  }
+  return targetObj;
+}
+
+
+// concat 不会修改原数组 返回一个新数组 concat 只是对数组的第一层进行深拷贝
+// slice 同上
+// Object.assign()
+// ...
+// 赋值运算符 = 实现的是浅拷贝，只拷贝对象的引用值；
+// JavaScript 中数组和对象自带的拷贝方法都是“首层浅拷贝”；
+// JSON.stringify 实现的是深拷贝，但是对目标对象有要求；
+// 若想真正意义上的深拷贝，请递归。
